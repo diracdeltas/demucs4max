593f6d355b0a87906978981ca315e1ce580644b8
yan <yan@brave.com>
2022-05-25 14:10:39 -0700

    attempt to handle user installations

--- demucs.js
+++ demucs.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const Max = require('max-api')
-const { exec, execSync } = require('child_process')
+const { exec, execSync, spawnSync } = require('child_process')
 const done = () => {
   Max.outlet('spleeterDone')
 }
 
 const MODEL_NAME = 'mdx_extra_q'
+const isWindows = process.platform === 'win32'
+const PYTHON = isWindows ? 'python' : 'python3'
 
 Max.post(`Loaded the ${path.basename(__filename)} script`)
 // Docker's default path may not be in Max Node's env path
@@ -28,28 +30,29 @@ Max.addHandlers({
 const showDir = (dir) => {
   // Since LOM has no way to load these files automatically into new tracks,
   // just open a file dialog and let the user drag-and-drop them.
-  let opener
-  if (process.platform === 'darwin') {
-    opener = 'open'
-  } else if (process.platform === 'win32') {
-    opener = 'start ""'
-  } else {
-    Max.post(`Unsupported platform: ${process.platform}`)
-  }
+  const opener = isWindows ? 'start ""' : 'open'
   execSync(`${opener} "${dir}"`)
   Max.outlet('set', `Select a clip; then press the button to start.`)
 }
 
+const demucsInPath = () => {
+  Max.outlet('set', 'Checking demucs installation...')
+  const which = isWindows ? : 'where' : 'which'
+  // Return true if demucs is in the path
+  return spawnSync(which, ['demucs']).status === 0
+}
+
 const runSpleeter = (filename) => {
-  const cmd = `demucs -n ${MODEL_NAME} "${filename}"`
+  let cmd = `demucs -n ${MODEL_NAME} "${filename}"`
+  cmd = demucsInPath() ? cmd : `${PYTHON} -m ${cmd}`
   Max.outlet('set', `Demucs is running. This will take a while...`)
   Max.post(cmd)
 
   // Calls the spleeter python process
   exec(cmd, (err, stdout, stderr) => {
     if (err) {
-      Max.outlet('set', `Error: ${err.message}`)
-      Max.post(`Error: ${err.message}`)
+      Max.outlet('set', err.message)
+      Max.post(err.message)
       Max.post(`Spleeter stderr: ${stderr}`)
       Max.post(`Spleeter stdout: ${stdout}`)
       done()
